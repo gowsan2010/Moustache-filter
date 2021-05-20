8d28c1c7c727dceca5bc60728966eebc9b4f0fb1
gowsan2010 <77194511+gowsan2010@users.noreply.github.com>
2021-05-20 11:18:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-function preload(){
-}
 let moustache;
+function preload(){
+    moustache = loadImage("https://i.postimg.cc/rstHNLXf/m.png");
+}
 function setup(){
     canvas = createCanvas(300, 300);
-    canvas.center(); 
-    moustache = loadImage("filter.png");  
+    canvas.center();   
     video = createCapture(VIDEO);
     video.size(300,300);
     video.hide(); 
@@ -13,7 +13,7 @@ function draw(){
     image(video, 0, 0, 300, 300);
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
-    image(moustache, nose_x-30, nose_y, 50, 20);
+    image(moustache, nose_x-20, nose_y, 50, 20);
 }
 function modelLoaded(){
     console.log("ml5.js is loaded");
